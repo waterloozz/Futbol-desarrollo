32e5df0393d671080515d89745afe5cd3ca3bb63
Carlos <carlos.benavides@mail.udp.cl>
2026-09-17 11:20:51 -0300

    Login funcional con roles de admin y arbitro

--- login.js
+++ login.js
@@ -1,2 +1,33 @@
 const form = document.getElementById("formLogin");
-console.log(form);
+const mensaje = document.getElementById("mensaje");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const usuario = document.getElementById("usuario").value;
+  const password = document.getElementById("password").value;
+
+  if (usuario === "arbitro") {
+    if (password === "arbitro123") {
+      mensaje.textContent = "Bienvenido";
+      mensaje.style.color = "green";
+      localStorage.setItem("rol", "arbitro");
+      // Aquí luego pasaremos a la pantalla de partidos
+    } else {
+      mensaje.textContent = "Contraseña incorrecta";
+      mensaje.style.color = "red";
+    }
+  } else if (usuario === "admin") {
+    if (password === "admin123") {
+      mensaje.textContent = "Bienvenido";
+      mensaje.style.color = "green";
+      localStorage.setItem("rol", "admin");
+    } else {
+      mensaje.textContent = "Contraseña incorrecta";
+      mensaje.style.color = "red";
+    }
+  } else {
+    mensaje.textContent = "Usuario no reconocido.";
+    mensaje.style.color = "red";
+  }
+});
